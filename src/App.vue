7c6f0626f460a0a95e2ea6ba8c46c110443ea842
Karma Blackshaw <irenesejah29@gmail.com>
2022-03-01 14:32:26 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app">
+    <TheNavbar class="app__navbar" />
+
+    <div class="app__body">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import TheNavbar from '@/components/TheNavbar/TheNavbar'
+
+export default {
+  components: {
+    TheNavbar
+  },
+  setup () {
+
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  height: 100%;
+  min-height: 100vh;
+}
+</style>
---
+++ src/components/TheNavbar/TheNavbar.vue
@@ -0,0 +1,60 @@
+<template>
+  <nav class="navbar-container">
+    <div class="navbar">
+      <div class="navbar__title">
+        <img
+          src="./images/logo.png"
+          alt="Logo"
+          class="navbar__logo"
+        >
+
+        DesireHunt
+      </div>
+
+      <div class="navbar__link">
+        <ul>
+          <li class="link__item">
+            <router-link to="/">
+              Home
+            </router-link>
+          </li>
+          <li class="link__item">
+            <router-link to="/services">
+              Services
+            </router-link>
+          </li>
+          <li class="link__item">
+            <router-link to="/about-us">
+              About Us
+            </router-link>
+          </li>
+          <li class="link__item">
+            <router-link to="/blog">
+              Blog
+            </router-link>
+          </li>
+        </ul>
+      </div>
+
+      <div class="navbar__append">
+        <div class="append__language">
+          Language
+        </div>
+
+        <div class="append__contact">
+          Contact
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'TheNavbar'
+}
+</script>
+
+<style lang="scss" scoped>
+@import './styles/the-navbar';
+</style>
